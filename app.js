6b008f05a3925c86a045519ef87bc167bf169213
JordanKolev <jordan.g.kolev@gmail.com>
2022-12-15 21:46:06 +0200

    Initial game dev

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import { Player } from './player.js';
+import { InputHandler } from './input.js';
+
 window.addEventListener('load', function () { // adding load event in order to make javascript wait to load all dependent recourses (images, etc.) before initialization.
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d'); // ctx is short for context, getContext() is a method
@@ -5,5 +8,32 @@ window.addEventListener('load', function () { // adding load event in order to m
     // resize canvas:
     canvas.width = 500;
     canvas.height = 500;
+
+    class Game {
+        constructor(width, height) {
+            this.width = width;
+            this.height = height;
+            this.player = new Player(this);
+            this.input = new InputHandler();
+        }
+
+        update() {
+            this.player.update();
+        }
+        draw(context) {
+            this.player.draw(context);
+        }
+    }
+
+    const game = new Game(canvas.width, canvas.height);
+    console.log(game);
+
+    function animate() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        game.update();
+        game.draw(ctx);
+        requestAnimationFrame(animate);
+    }
+    animate();
 
 });
